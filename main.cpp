fa3c21cf2ad16788950ed1c40f46ca3c2c01c2d1
Reets123 <ibrahimalatig@gmail.com>
2024-09-24 14:01:53 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Lab 8: Github Repository
+// COMSC-210 - Ibrahim Alatig
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Simple program" << endl;
+
+    return 0;
+
+}
